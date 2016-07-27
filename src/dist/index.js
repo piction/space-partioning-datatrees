4151021ac2bc48fd3df1d46931ccb129cb0fd67a
jonas.vervoort@egemin.be <2284@ecorp.int>
2016-07-27 15:42:22 +0200

    changed output path library

--- src/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var node_1 = require("./node");
-exports.Node = node_1.Node;
-var binaryHeap_1 = require("./binaryHeap");
-exports.BinaryHeap = binaryHeap_1.BinaryHeap;
-var kdTree_1 = require("./kdTree");
-exports.KdTree = kdTree_1.KdTree;
-//# sourceMappingURL=index.js.map
